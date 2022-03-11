d7822ffe9b7479c25285939b440c2f3fce72443e
SavaIv <tie.mail@abv.bg>
2022-03-12 00:54:47 +0200

    Login View - 1st edition

--- WarehouseSoftUni/Controllers/HomeController.cs
+++ WarehouseSoftUni/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using WarehouseSoftUni.Models;
 
 namespace WarehouseSoftUni.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         private readonly ILogger<HomeController> _logger;
 
