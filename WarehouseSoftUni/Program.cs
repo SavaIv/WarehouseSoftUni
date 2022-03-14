5bf17f4b3dd4e53c873ab972131db5d3dcb8d4e1
SavaIv <tie.mail@abv.bg>
2022-03-14 21:57:28 +0200

    Adding Repository

--- WarehouseSoftUni/Program.cs
+++ WarehouseSoftUni/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Warehouse.Core.Constants;
 using Warehouse.Infrastructure.Data;
 using WarehouseSoftUni.ModelBinders;
+using Warehouse.Infrastructure.Data.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,6 +22,8 @@ builder.Services.AddControllersWithViews()
         options.ModelBinderProviders.Insert(1, new DateTimeModelBinderProvider(FormatingConstant.NormalDateFormat));
         options.ModelBinderProviders.Insert(2, new DoubleModelBinderProvider());
     });
+
+builder.Services.AddScoped<IApplicatioDbRepository, ApplicatioDbRepository>();
 
 var app = builder.Build();
 
