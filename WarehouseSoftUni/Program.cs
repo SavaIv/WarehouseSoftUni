3d1b15c6391b4eef467c3c2ebe61a909b7cb0bbe
SavaIv <tie.mail@abv.bg>
2022-04-07 23:58:48 +0300

    Add cookie consent policy

--- WarehouseSoftUni/Program.cs
+++ WarehouseSoftUni/Program.cs
@@ -30,6 +30,16 @@ builder.Services.AddAuthentication()
         options.AppSecret = builder.Configuration.GetValue<string>("Facebook:AppSecret");
     });
 
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    // This lambda determines whether user consent for non-essential 
+    // cookies is needed for a given request.
+    options.CheckConsentNeeded = context => true;
+    // requires using Microsoft.AspNetCore.Http;
+    options.MinimumSameSitePolicy = SameSiteMode.None;
+});
+
+
 builder.Services.AddControllersWithViews()
     .AddMvcOptions(options =>
     {
@@ -60,6 +70,7 @@ app.UseStatusCodePages();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+app.UseCookiePolicy();
 
 app.UseRouting();
 
